962071669033c615ec17ed602c54ab76c33ff24b
Mela-pq <melqui36927@gmail.com>
2026-02-05 18:19:37 -0500

    Virus

---
+++ README.md
@@ -0,0 +1,20 @@
+Nombre:Melany Quilumba 
+
+¿Qué es?
+Es un sistema que nos permite extraer las entradas del teclado .
+
+¿Comò se usa?
+En la terminal colocamos el siguiente comando.
+
+![alt text](image.png)
+
+Pruebas :
+
+Usamos el comando de ejecucion.
+![alt text](image-1.png)
+
+pausamos el sistema y con el comando cat revisamos el contenido de events.txt
+![alt text](image-2.png)
+
+
+
---
+++ main.py
@@ -0,0 +1,28 @@
+import keyboard
+#importamos la libreria
+# Graba los eventos del teclado hasta que se presione ESC
+recorded = keyboard.record(until='esc')
+#variable igual a teclado .record graba las teclas y el util 
+# permite que se termine el programa con esc
+# Reproduce los eventos grabados a tres veces la velocidad
+keyboard.play(recorded)
+#del teclado encendido recoberes llama a la
+# variable escritura rapida de lo que ingrese 
+
+
+
+
+
+
+
+
+            
+        
+
+    
+
+
+
+
+
+
